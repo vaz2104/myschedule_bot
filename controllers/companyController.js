9e5eae7a2fd2ac062fefa6f9ce0a53f04d5ded15
vaz2104 <s.manuliak@gmail.com>
2024-12-06 23:07:44 +0200

    refactored the company routes logic

---
+++ controllers/companyController.js
@@ -0,0 +1,43 @@
+const CompanyService = require("../services/companyService");
+
+class CompanyController {
+  async create(req, res) {
+    try {
+      const company = await CompanyService.create(req.body);
+      res.json(company);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+
+  async getAllOrCompanyId(req, res) {
+    try {
+      const companies = await CompanyService.getAllOrCompanyId(
+        req.query?.userId
+      );
+      res.json(companies);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+
+  async getOne(req, res) {
+    try {
+      const company = await CompanyService.getOne(req.params?.id);
+      res.json(company);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const company = await CompanyService.update(req.body);
+      res.json(company);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+}
+
+module.exports = new CompanyController();
