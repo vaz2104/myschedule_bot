0a703a7ffa8d5b24f3588640ce7122892d267bdc
vaz2104 <s.manuliak@gmail.com>
2025-08-23 22:05:05 +0300

    refactoring

--- routs/v.2/botRouts.js
+++ routs/v.2/botRouts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const BotController = require("../../controllers/v.2/botController");
+const BotController = require("../../controllers/v.2/BotController");
 
 router.get("/bot/:token", BotController.getInfo);
 router.get("/bot/client-photo", BotController.getClientPhoto);
