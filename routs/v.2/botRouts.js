6521c2ca45d2357bff9a1ec8a764f0982cd53bab
vaz2104 <s.manuliak@gmail.com>
2025-11-09 18:05:36 +0200

    fixing bugs

--- routs/v.2/botRouts.js
+++ routs/v.2/botRouts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const BotController = require("../../controllers/v.2/BotController");
+const BotController = require("../../controllers/v.2/botController");
 
 router.get("/bot/:token", BotController.getInfo);
 router.get("/bot/client-photo", BotController.getClientPhoto);
